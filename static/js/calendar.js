5923b322795fd5326fc7994242f409c52659eaf4
ks-moss <k.sinamkam.usa@gmail.com>
2024-06-19 12:39:52 -0700

    Update

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -12,121 +12,133 @@ const MONTHS = [
 
 // //////// BY YEAR /////////////
 
-// Function to make AJAX request to get calendar data
-function getCalendarByYear(value) {
-    return new Promise((resolve, reject) => {
-        const formData = new FormData();
+// // Function to make AJAX request to get calendar data
+// function getCalendarByYear(value) {
+//     return new Promise((resolve, reject) => {
+//         const formData = new FormData();
 
-        value.years.forEach(years => formData.append('years', years));
+//         value.years.forEach(years => formData.append('years', years));
 
-        $.ajax({
-            url: '/get_calendar_year',
-            type: 'POST',
-            processData: false,
-            contentType: false,
-            data: formData,
-            success: response => resolve(response),
-            error: reject
-        });
-    });
-}
+//         $.ajax({
+//             url: '/get_calendar_year',
+//             type: 'POST',
+//             processData: false,
+//             contentType: false,
+//             data: formData,
+//             success: response => resolve(response),
+//             error: reject
+//         });
+//     });
+// }
 
 
 
-function generateCalendarForYear(response) {
-    var calendarHTML = ''; // Initialize an empty string to store the HTML
+// function generateCalendarForYear(response) {
+//     var calendarHTML = ''; // Initialize an empty string to store the HTML
 
-    // Days of the week
-    var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//     // Days of the week
+//     var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    // Append HTML for days of the week
+//     // Append HTML for days of the week
     
-    // Iterate through the response data
-    for (var i = 0; i < response.length; i++) {
+//     // Iterate through the response data
+//     for (var i = 0; i < response.length; i++) {
 
-        for (var j = 0; j < response[i].length; j++) {
+//         for (var j = 0; j < response[i].length; j++) {
             
-            var getYearMonth = response[i][j][0].split(",");
-            var year = parseInt(getYearMonth[3]);
-            var month = getYearMonth[2];
+//             var getYearMonth = response[i][j][0].split(",");
+//             var year = parseInt(getYearMonth[3]);
+//             var month = getYearMonth[2];
         
-            // Append year-month div inside the table
-            calendarHTML += '<table class="calendar"> <caption class="year-month">' + year + " - " + month +'</caption>'
-            calendarHTML += '<thead><tr>';
+//             // Append year-month div inside the table
+//             calendarHTML += '<table class="calendar"> <caption class="year-month">' + year + " - " + month +'</caption>'
+//             calendarHTML += '<thead><tr>';
         
-            daysOfWeek.forEach(function(day) {
-                calendarHTML += '<th>' + day + '</th>';
-            });
-            calendarHTML += '</tr></thead><tbody>';
+//             daysOfWeek.forEach(function(day) {
+//                 calendarHTML += '<th>' + day + '</th>';
+//             });
+//             calendarHTML += '</tr></thead><tbody>';
 
 
         
-            for(var k=0; k<response[i][j].length; k++){
+//             for(var k=0; k<response[i][j].length; k++){
 
-                var parts = response[i][j][k].split(",");
+//                 var parts = response[i][j][k].split(",");
        
-                var dayOfWeek = parseInt(parts[0]);
-                var day = parseInt(parts[1]);
+//                 var dayOfWeek = parseInt(parts[0]);
+//                 var day = parseInt(parts[1]);
 
 
-                if (dayOfWeek === 0) {
-                    calendarHTML += '<tr>';
-                }
+//                 if (dayOfWeek === 0) {
+//                     calendarHTML += '<tr>';
+//                 }
 
-                if (dayOfWeek > 0 && day === 1) {
-                    for (var l = 0; l < dayOfWeek; l++) {
-                        calendarHTML += '<td></td>';
-                    }
-                }
+//                 if (dayOfWeek > 0 && day === 1) {
+//                     for (var l = 0; l < dayOfWeek; l++) {
+//                         calendarHTML += '<td></td>';
+//                     }
+//                 }
 
-                calendarHTML += '<td>' + day + '</td>';
+//                 // Add data attributes to the td element
+//                 calendarHTML += '<td data-year="' + year + '" data-month="' + month + '" data-day="' + day + '">' + day + '</td>';
 
-                if (dayOfWeek === 6) {
-                    calendarHTML += '</tr>';
-                }
+//                 if (dayOfWeek === 6) {
+//                     calendarHTML += '</tr>';
+//                 }
 
-                if (k === response[i][j].length - 1 && dayOfWeek < 6) {
+//                 if (k === response[i][j].length - 1 && dayOfWeek < 6) {
                     
-                    for (var m = dayOfWeek; m < 6; m++) {
-                        calendarHTML += '<td></td>';
-                    }
-                    calendarHTML += '</tr>';
-                }
+//                     for (var m = dayOfWeek; m < 6; m++) {
+//                         calendarHTML += '<td></td>';
+//                     }
+//                     calendarHTML += '</tr>';
+//                 }
    
-            }
+//             }
 
-            calendarHTML += '</tbody></table>';
+//             calendarHTML += '</tbody></table>';
 
-        }
+//         }
 
         
-    }
+//     }
     
-    return calendarHTML; // Return the generated HTML
-}
+//     return calendarHTML; // Return the generated HTML
+// }
 
 
 
 
-// Main function for fetching data by year and generating calendar
-async function fetchAndGenerateCalendarByYear(years) {
-    const years_input = { years };
+// // Main function for fetching data by year and generating calendar
+// async function fetchAndGenerateCalendarByYear(years) {
+//     const years_input = { years };
 
-    try {
-        const response = await getCalendarByYear(years_input);
+//     try {
+//         const response = await getCalendarByYear(years_input);
         
-        var calendarHTML = generateCalendarForYear(response);
-        $('.calendar-container').append(calendarHTML);
+//         var calendarHTML = generateCalendarForYear(response);
+//         $('.calendar-container').html(calendarHTML);
+
+//         // Attach click event listeners to each date cell
+//         $('.calendar td').on('click', function() {
+//             var year = $(this).data('year');
+//             var month = $(this).data('month');
+//             var day = $(this).data('day');
+//             if (day) { // Ensure the cell is not empty
+//                 console.log(year, month, day);
+//             }
+//         });
+
         
-    } catch (error) {
-        console.error("Async Error:", error);
-    }
-}
+//     } catch (error) {
+//         console.error("Async Error:", error);
+//     }
+// }
 
 
-// Usage
-const years = [2024, 2025];
-fetchAndGenerateCalendarByYear(years);
+// // Usage
+// const years = [2024, 2025];
+// fetchAndGenerateCalendarByYear(years);
 
 
 
@@ -139,130 +151,119 @@ fetchAndGenerateCalendarByYear(years);
 
 ////////// BY MONTH /////////////
 
-// // Function to make AJAX request to get calendar data
-// function getCalendarByMonth(value) {
-//     return new Promise((resolve, reject) => {
-//         const formData = new FormData();
-//         formData.append('year', value.year);
-//         formData.append('month', value.month);
+// Function to make AJAX request to get calendar data
+function getCalendarByMonth(value) {
+    return new Promise((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('year', value.year);
+        formData.append('month', value.month);
         
-//         $.ajax({
-//             url: '/get_calendar_month',
-//             type: 'POST',
-//             processData: false,
-//             contentType: false,
-//             data: formData,
-//             success: response => resolve(response),
-//             error: reject
-//         });
-//     });
-// }
-
-// // Function to generate the calendar header
-// function generateCalendarHeader() {
-//     // Array containing the days of the week
-//     var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-//     // Get the table head element
-//     var calendarHead = $("#calebdar-head");
-
-//     // Create a table row element
-//     var tr = $("<tr>");
-
-//     // Loop through the daysOfWeek array to create th elements
-//     daysOfWeek.forEach(function(day) {
-//         // Create a th element for each day
-//         var th = $("<th>").text(day);
-//         // Append the th element to the table row
-//         tr.append(th);
-//     });
-
-//     // Append the table row to the table head
-//     calendarHead.append(tr);
-// }
+        $.ajax({
+            url: '/get_calendar_month',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: formData,
+            success: response => resolve(response),
+            error: reject
+        });
+    });
+}
 
 
+function generateCalendarForYear(response) {
+    var calendarHTML = ''; // Initialize an empty string to store the HTML
 
-// function generateCalendarForYear(response) {
-//     var calendarHTML = ''; // Initialize an empty string to store the HTML
+    // Days of the week
+    var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-//     // Days of the week
-//     var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var getYearMonth = response[0].split(",");
+    var year = parseInt(getYearMonth[3]);
+    var month = getYearMonth[2];
 
-//     var getYearMonth = response[0].split(",");
-//     var year = parseInt(getYearMonth[3]);
-//     var month = getYearMonth[2];
+    // Append year-month div inside the table
+    calendarHTML += '<table class="calendar"> <caption class="year-month">' + year + " - " + month +'</caption>'
+    calendarHTML += '<thead><tr>';
 
-//     // Append year-month div inside the table
-//     calendarHTML += '<table class="calendar"> <caption class="year-month">' + year + " - " + month +'</caption>'
-//     calendarHTML += '<thead><tr>';
-
-//     daysOfWeek.forEach(function(day) {
-//         calendarHTML += '<th>' + day + '</th>';
-//     });
-//     calendarHTML += '</tr></thead><tbody>';
+    daysOfWeek.forEach(function(day) {
+        calendarHTML += '<th>' + day + '</th>';
+    });
+    calendarHTML += '</tr></thead><tbody>';
     
     
-//     for (var i = 0; i < response.length; i++) {
+    for (var i = 0; i < response.length; i++) {
 
-//         var parts = response[i].split(",");
-//         var dayOfWeek = parseInt(parts[0]);
-//         var day = parseInt(parts[1]);
+        var parts = response[i].split(",");
+        var dayOfWeek = parseInt(parts[0]);
+        var day = parseInt(parts[1]);
     
 
-//         if (dayOfWeek === 0) {
-//             calendarHTML += '<tr>';
-//         }
+        if (dayOfWeek === 0) {
+            calendarHTML += '<tr>';
+        }
 
         
 
-//         if (dayOfWeek > 0 && day === 1) {
-//             for (var j = 0; j < dayOfWeek; j++) {
-//                 calendarHTML += '<td></td>';
+        if (dayOfWeek > 0 && day === 1) {
+            for (var j = 0; j < dayOfWeek; j++) {
+                calendarHTML += '<td></td>';
                 
-//             }
-//         }
+            }
+        }
 
-//         calendarHTML += '<td>' + day + '</td>';
+        // Add data attributes to the td element
+        calendarHTML += '<td data-year="' + year + '" data-month="' + month + '" data-day="' + day + '">' + day + '</td>';
 
-//         if (dayOfWeek === 6) {
-//             calendarHTML += '</tr>';
-//         }
+        if (dayOfWeek === 6) {
+            calendarHTML += '</tr>';
+        }
 
-//         if (i === response.length - 1 && dayOfWeek < 6) {
+        if (i === response.length - 1 && dayOfWeek < 6) {
             
-//             for (var m = dayOfWeek; m < 6; m++) {
-//                 calendarHTML += '<td></td>';
-//             }
-//             calendarHTML += '</tr>';
-//         }
+            for (var m = dayOfWeek; m < 6; m++) {
+                calendarHTML += '<td></td>';
+            }
+            calendarHTML += '</tr>';
+        }
    
 
-//     }
+    }
 
-//     calendarHTML += '</tbody></table>';
+    calendarHTML += '</tbody></table>';
     
-//     return calendarHTML; // Return the generated HTML
-// }
+    return calendarHTML; // Return the generated HTML
+}
 
 
-// // Main function for fetching data by month and generating calendar
-// async function fetchAndGenerateCalendarByMonth(data_input) {
-//     try {
-//         const response = await getCalendarByMonth(data_input);
+// Main function for fetching data by month and generating calendar
+async function fetchAndGenerateCalendarByMonth(data_input) {
+    try {
+        const response = await getCalendarByMonth(data_input);
 
-//         var calendarHTML = generateCalendarForYear(response);
-//         $('.calendar-container').append(calendarHTML);
-//     } catch (error) {
-//         console.error("Async Error:", error);
-//     }
-// }
+        var calendarHTML = generateCalendarForYear(response);
+        $('.calendar-container').html(calendarHTML);
+
+        // Attach click event listeners to each date cell
+        $('.calendar td').on('click', function() {
+            var year = $(this).data('year');
+            var month = $(this).data('month');
+            var day = $(this).data('day');
+            if (day) { // Ensure the cell is not empty
+                console.log(year, month, day);
+            }
+        });
+
+        
+    } catch (error) {
+        console.error("Async Error:", error);
+    }
+}
 
 
-// // Usage
-// const data_input = {
-//     year: 2024,
-//     month: "May"
-// }
+// Usage
+const data_input = {
+    year: 2024,
+    month: "May"
+}
 
-// fetchAndGenerateCalendarByMonth(data_input);
+fetchAndGenerateCalendarByMonth(data_input);
