3dfb39deb0904adf21c4ac940c192332da8ee57a
ks-moss <k.sinamkam.usa@gmail.com>
2024-06-20 15:33:58 -0700

    Update

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -149,7 +149,193 @@ const MONTHS = [
 
 
 
-////////// BY MONTH /////////////
+// ////////// BY MONTH /////////////
+
+
+function generateYears(currentYear) {
+
+    minus_year = currentYear - 4;
+
+    if(minus_year < 1500){
+        minus_year = 1500;
+    }
+
+    plus_year = currentYear + 4;
+
+    temp = [];
+
+    for(var i=minus_year; i<=plus_year; i++){
+        temp.push(i);
+    }
+   
+    return temp;
+}
+
+
+
+
+function displayYears(currentYear) {
+
+
+    YEARS_OPTIONS = generateYears(currentYear);
+
+    console.log(YEARS_OPTIONS);
+
+    let yearsHtml = '<div class="years-selection">';
+    
+    for (let i = 0; i < YEARS_OPTIONS.length; i++) {
+
+        if(i % 3 === 0) {
+            yearsHtml += '<div class="years-row">'
+        }
+        
+        yearsHtml += `<div class="years-options" data-year="${YEARS_OPTIONS[i]}">${YEARS_OPTIONS[i]}</div>`;
+
+        if ((i + 1) % 3 === 0) {
+            yearsHtml += '</div>';
+        }
+    
+    }
+
+    // Close the last row if it wasn't closed already
+    if (YEARS_OPTIONS.length % 3 !== 0) {
+        yearsHtml += '</div>';
+    }
+
+    yearsHtml += '</div>';
+
+
+    const $yearSelection = $(
+
+        '<div class="years-display">' +
+            '<div class="arrow left">&lt;</div>' + 
+            YEARS_OPTIONS[0] + " - " + YEARS_OPTIONS[YEARS_OPTIONS.length-1] + 
+            '<div class="arrow right">&gt;</div>' +
+        '</div>' + yearsHtml
+
+
+    );
+
+    $('.years-container').html($yearSelection);
+
+    $('.years-container').addClass('display-flex');
+
+    
+
+    // Attach click event listeners to each year option
+    $('.years-options').on('click', function() {
+        const selectedYear = $(this).data('year');
+        console.log(selectedYear);
+     
+        $('.years-container').removeClass('display-flex');
+        $('.years-container').addClass('display-none');
+    
+        displayMonths(selectedYear);
+    });
+
+    // Attach click event listeners to the arrow buttons
+    let temp_year = currentYear;
+    $('.arrow.left').on('click', function() {
+       
+        if(YEARS_OPTIONS[0] > 1500){
+            temp_year -= 8;
+            displayYears(temp_year); 
+        }
+        else{
+            console.error("Error: Year cannot be LOWER than 1500. Current Index =", YEARS_OPTIONS[0]);
+        }
+        
+    });
+    $('.arrow.right').on('click', function() {
+        temp_year += 8;
+        displayYears(temp_year);
+    });
+
+}
+
+
+
+
+
+
+
+
+function displayMonths(selectedYear) {
+
+
+    let monthsHtml = '<div class="months-selection">';
+    
+    for (let i = 0; i < MONTHS.length; i++) {
+
+        if(i % 3 === 0) {
+            monthsHtml += '<div class="months-row">'
+        }
+        
+        monthsHtml += `<div class="months-options" data-month="${MONTHS[i]}">${MONTHS[i]}</div>`;
+
+        if ((i + 1) % 3 === 0) {
+            monthsHtml += '</div>';
+        }
+    
+    }
+
+    // Close the last row if it wasn't closed already
+    if (MONTHS.length % 3 !== 0) {
+        monthsHtml += '</div>';
+    }
+
+    monthsHtml += '</div>';
+
+    const $monthSelection = $(
+
+        '<div class="yearMonths-display">' +
+            selectedYear +
+        '</div>' + monthsHtml
+
+
+    );
+
+    $('.months-container').html($monthSelection);
+
+    $('.months-container').removeClass('display-none');
+    $('.months-container').addClass('display-flex');
+
+    $('.months-options').on('click', function() {
+        const selectedMonth = $(this).data('month');
+        console.log(selectedMonth);
+
+        // Usage
+        const data_input = {
+            year: selectedYear,
+            month: selectedMonth
+        }
+
+        $('.months-container').removeClass('display-flex');
+        $('.months-container').addClass('display-none');
+        
+        fetchAndGenerateCalendarByMonth(data_input);
+
+    });
+
+    $('.yearMonths-display').on('click', function() {
+        
+        $('.months-container').removeClass('display-flex');
+        $('.months-container').addClass('display-none');
+
+        displayYears(selectedYear);
+
+    });
+
+    
+
+
+}
+
+
+
+
+
+
 
 // Function to make AJAX request to get calendar data
 function getCalendarByMonth(value) {
@@ -182,7 +368,7 @@ function generateCalendarForYear(response) {
     var month = getYearMonth[2];
 
     // Append year-month div inside the table
-    calendarHTML += '<table class="calendar"> <caption class="year-month">' + year + " - " + month +'</caption>'
+    calendarHTML += '<table class="calendar"> <caption class="year-month" data-year="' + year + '">' + year + " - " + month +'</caption>';
     calendarHTML += '<thead><tr>';
 
     daysOfWeek.forEach(function(day) {
@@ -238,10 +424,21 @@ function generateCalendarForYear(response) {
 // Main function for fetching data by month and generating calendar
 async function fetchAndGenerateCalendarByMonth(data_input) {
     try {
+
+        $('.years-container').removeClass('display-flex');
+        $('.years-container').addClass('display-none');
+
+        $('.months-container').removeClass('display-flex');
+        $('.months-container').addClass('display-none');
+
+        
+        
+       
         const response = await getCalendarByMonth(data_input);
 
         var calendarHTML = generateCalendarForYear(response);
         $('.calendar-container').html(calendarHTML);
+        $('.calendar-container').addClass('display-flex');
 
         // Attach click event listeners to each date cell
         $('.calendar td').on('click', function() {
@@ -253,6 +450,17 @@ async function fetchAndGenerateCalendarByMonth(data_input) {
             }
         });
 
+        $('.calendar caption').on('click', function() {
+            var selectedYear = $(this).data('year');
+            console.log(selectedYear);
+
+            $('.calendar-container').removeClass('display-flex');
+            $('.calendar-container').addClass('display-none');
+
+            displayMonths(selectedYear)
+        
+        });
+
         
     } catch (error) {
         console.error("Async Error:", error);
@@ -260,10 +468,20 @@ async function fetchAndGenerateCalendarByMonth(data_input) {
 }
 
 
-// Usage
-const data_input = {
-    year: 2024,
-    month: "May"
-}
 
+// Create a new Date object
+const currentDate = new Date();
+
+// Get the individual components of the date
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Months are zero-based
+const day = currentDate.getDate();
+
+console.log(year + "-" + month + "-"  + day); // Output the formatted date
+
+
+const data_input = {
+    year: year,
+    month: MONTHS[month-1]
+}
 fetchAndGenerateCalendarByMonth(data_input);
